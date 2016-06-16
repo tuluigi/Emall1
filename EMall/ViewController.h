371bb010a556f49c7f2cdaf6c7ffc474f85703bb
tujinguo <tujinguo@corp.netease.com>
2016-06-16 19:26:33 +0800

    Initial Commit

---
+++ EMall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EMall
+//
+//  Created by Luigi on 16/6/16.
+//  Copyright © 2016年 Luigi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EMall/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EMall
+//
+//  Created by Luigi on 16/6/16.
+//  Copyright © 2016年 Luigi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
