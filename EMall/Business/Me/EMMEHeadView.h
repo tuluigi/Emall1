36c36fb1165cee4e321e5e4831cbb5333245af35
tujinguo <tujinguo@corp.netease.com>
2016-07-04 19:30:31 +0800

    add file

---
+++ EMall/Business/Me/EMMEHeadView.h
@@ -0,0 +1,13 @@
+//
+//  EMMEHeadView.h
+//  EMall
+//
+//  Created by Luigi on 16/6/22.
+//  Copyright © 2016年 Luigi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMMEHeadView : UIView
+- (void)setUserName:(NSString *)userName headImageUrl:(NSString *)headImageUrl;
+@end
---
+++ EMall/Business/Me/EMMEHeadView.m
@@ -0,0 +1,28 @@
+//
+//  EMMEHeadView.m
+//  EMall
+//
+//  Created by Luigi on 16/6/22.
+//  Copyright © 2016年 Luigi. All rights reserved.
+//
+
+#import "EMMEHeadView.h"
+
+@interface EMMEHeadView ()
+@property(nonatomic,strong)UIImageView *headImageView;
+@property(nonatomic,strong)UILabel *nameLable;
+@end
+
+@implementation EMMEHeadView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+- (void)setUserName:(NSString *)userName headImageUrl:(NSString *)headImageUrl{
+    
+}
+@end
