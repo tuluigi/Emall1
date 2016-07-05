426a25a835ac835adda197e111d7e3bc9c67f4ba
tujinguo <tujinguo@corp.netease.com>
2016-07-05 22:34:43 +0800

    aadd

--- EMall/Business/Home/EMHomeGoodsCell.m
+++ EMall/Business/Home/EMHomeGoodsCell.m
@@ -15,8 +15,8 @@
 
 @implementation EMHomeGoodsCell
 
-- (instancetype)init{
-    self=[super init];
+- (instancetype)initWithFrame:(CGRect)frame{
+    self=[super initWithFrame:frame];
     if (self) {
         [self onInitContentView];
     }
