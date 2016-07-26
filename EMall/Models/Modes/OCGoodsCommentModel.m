d82abed633e29c4a7a0065c6ec591e096af8a945
tuluigi <tujinguo@corp.netease.com>
2016-07-26 23:26:06 +0800

    add file

---
+++ EMall/Models/Modes/OCGoodsCommentModel.m
@@ -0,0 +1,13 @@
+//
+//  OCGoodsCommentModel.m
+//  EMall
+//
+//  Created by Luigi on 16/7/26.
+//  Copyright © 2016年 Luigi. All rights reserved.
+//
+
+#import "OCGoodsCommentModel.h"
+
+@implementation OCGoodsCommentModel
+
+@end
